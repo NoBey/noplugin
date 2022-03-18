4b11bd8b38f670f319dd5b9cfcc443ffec778f64
shengpeiyin <shengpeiyin@kuaishou.com>
2022-03-18 14:06:12 +0800

    init

--- packages/view/src/index.tsx
+++ packages/view/src/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDom from 'react-dom';
 import { useName,  usePluginContext } from '@noplugin/core'
 
+const App = () => {
+    return <>App View</>
+}
 
 
+const createView = () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    ReactDom.render(<App />, div)
+}
 
-
-export default function (){
+export default function view(){
     const pluginContexty =  usePluginContext()
-    useName('view666')
+    createView()
 }
